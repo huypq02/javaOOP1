9575ec50d66d97cc976ae122dfac3dd298c8be43
Huy Pham <huypham0297@gmail.com>
2024-07-30 23:19:18 +0700

    fix: validator for students

--- src/main/java/vn/edu/cybersoft/Exercise1/OOPApplication.java
+++ src/main/java/vn/edu/cybersoft/Exercise1/OOPApplication.java
@@ -5,6 +5,9 @@ import vn.edu.cybersoft.Exercise1.validator.SinhVienValidator;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
+import static vn.edu.cybersoft.Exercise1.validator.SinhVienValidator.numberValidator;
+import static vn.edu.cybersoft.Exercise1.validator.SinhVienValidator.stringValidator;
+
 public class OOPApplication {
     public static void main(String[] args) {
         try {
@@ -20,18 +23,41 @@ public class OOPApplication {
 
     public static void nhap(SinhVien sv){
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Nhap ho va ten: ");
         sv.setHoTen(sc.nextLine());
+        if (!stringValidator(sv.getHoTen())){ // validate
+            System.out.println("Ho va ten khong duoc de trong");
+            System.exit(0);
+        }
+
         System.out.println("Nhap ma sinh vien: ");
         sv.setMaSinhVien(sc.nextLine());
+        if (!stringValidator(sv.getMaSinhVien())){ // validate
+            System.out.println("Ma sinh vien khong duoc de trong");
+            System.exit(0);
+        }
+
         System.out.println("Nhap diem toan: ");
         sv.setDiemToan(sc.nextDouble());
+        if (!numberValidator((sv.getDiemToan()))){ // validate
+            System.out.println("Diem toan phai tu 0 den 10");
+            System.exit(0);
+        }
+
         System.out.println("Nhap diem ly: ");
         sv.setDiemLy(sc.nextDouble());
+        if (!numberValidator((sv.getDiemLy()))){ // validate
+            System.out.println("Diem ly phai tu 0 den 10");
+            System.exit(0);
+        }
+
         System.out.println("Nhap diem hoa: ");
         sv.setDiemHoa(sc.nextDouble());
-        // validate
-        SinhVienValidator.validate(sv.getHoTen(), sv.getMaSinhVien(), sv.getDiemToan(), sv.getDiemLy(), sv.getDiemHoa());
+        if (!numberValidator((sv.getDiemHoa()))){ // validate
+            System.out.println("Diem hoa phai tu 0 den 10");
+            System.exit(0);
+        }
     }
 
     public static void xuat(SinhVien sv){
